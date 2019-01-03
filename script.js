dabd47c87f1f57b6c7efc7ba0471370f0327c167
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-03 12:24:15 +0100

    const

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const AGE = 31; // CONST only assigned once
+// AGE = 27; 
+console.log(AGE);
+
+// Reassigniing consts holding values are allright, but not the reference it's pointing to.
+const OBJ = {
+    age: 31
+}
+console.log(OBJ)
+OBJ.age = 27
+console.log(OBJ)
+
 // Let allows "Block Scoping"
 
-let age = 31;
-if (true) {
-    let age = 27;
-    console.log('Inside', age);
-}
+// let age = 31;
+// if (true) {
+//     let age = 27;
+//     console.log('Inside', age);
+// }
 
-console.log('Outside', age);
+// console.log('Outside', age);
