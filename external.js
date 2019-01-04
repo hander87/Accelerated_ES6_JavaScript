dbb21623410e740cefe17239d0fa62987377c983
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-04 11:26:11 +0100

    Basic Modular Setup

--- external.js
+++ external.js
@@ -1,1 +1,11 @@
-export let keyValue = 1000;
+let keyValue = 1000;
+function test() {
+    console.log('Tested');
+    keyValue = 2000;
+}
+
+let ab = "Some text";
+
+// Grouping exports
+export { keyValue, test };
+// export default ab; // Will be the primary export
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-import { keyValue } from './external.js';
+// Note Modules are always in "strict" mode
+import { keyValue as key, test } from './external.js'; // .js is only required by systemjs
+import * as imported from './external.js'; // .js is only required by systemjs
+import a from './external.js'; // Export "default" only has one key value. Can be named to ANYTHING
 
-console.log(keyValue);
+console.log(imported); // * wrapps all exported elements into an OBJECT
+
+console.log(key);
+test();
+console.log(key);
+console.log(a);
