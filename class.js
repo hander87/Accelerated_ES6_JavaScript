a3eac75cec5095ac20eb0c7c9460ef592b3c5438
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-04 12:08:40 +0100

    Getters and Setters

--- class.js
+++ class.js
@@ -12,13 +12,35 @@ class Helper {
 Helper.logTwice('Logged');
 
 class Person {
-    constructor(name) {
+    constructor(name, middleName) {
         this.name = name;
+        this._middleName = middleName;
     }
     greet() {
         console.log('Hello there, my name is ' + this.name + ' and I am ' + this.age);
     }
+
+    get middleName() {
+        return this._middleName.toUpperCase();
+    }
+
+    set middleName( value ) {
+        if (value.length > 2) {
+            this._middleName = value;
+        } else {
+            console.log('Rejected the name ')
+        }
+    }
+
 }
+
+let person = new Person('Hannes', 'Johannes');
+
+console.log('GETTERS AND SETTERS:', person);
+
+person.middleName = 'J';
+console.log('Accessing the Getter (uppercase)', person.middleName);
+
 
 class Hannes extends Person {
     constructor(age) {
