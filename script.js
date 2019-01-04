33c1c54744fa6eb85c2704abf5894cd8cd624230
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-04 12:29:02 +0100

    new chapter setup

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// Note Modules are always in "strict" mode
-import { keyValue as key, test } from './external.js'; // .js is only required by systemjs
-import * as imported from './external.js'; // .js is only required by systemjs
-import a from './external.js'; // Export "default" only has one key value. Can be named to ANYTHING
-
-console.log('::MODULAR STRUCTURE::');
-console.log(imported); // * wrapps all exported elements into an OBJECT
-
-console.log(key);
-test();
-console.log(key);
-console.log(a);
+console.log('Script ready');
