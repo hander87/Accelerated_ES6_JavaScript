a305da2c481614bb971fce7634d32848f4bc81a0
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-06 10:44:49 +0100

    Symbol basic usage

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Script ready');
+console.log('::SYMBOLS::');
+// Type = Symbol
+let symbol = Symbol('debug'); // "debug" var only for id:ing/debugging
+let anotherSymbol = Symbol('debug'); // "debug" var only for id:ing/debugging
+console.log(symbol);
+
+let obj = {
+    name: 'hannes',
+    [symbol]: 22 // Used for metadata, where output is not needed
+}
+
+console.log(obj);
