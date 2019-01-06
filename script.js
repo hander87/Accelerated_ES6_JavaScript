31654833488a3c91324262db886d9ee61cb528ee
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-06 12:13:27 +0100

    ITERATOR SYMBOL

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Script running...');
+console.log('::ITERATOR SYMBOL::');
+let array = [1, 2, 3];
+
+// Iterator Symbol Type for checking if var is iterable
+let it = array[Symbol.iterator](); 
+
+// It is a object, that has function "next()" on it
+// calling next will "Iterate" through the variable
+console.log(it.next()); 
+console.log(it.next()); 
+console.log(it.next()); 
+console.log(it.next()); 
+console.log('value = the number of loops we´ve done');
+console.log('done = means if we´re done looping the array');
