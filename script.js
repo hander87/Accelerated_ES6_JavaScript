e6a5656552ae3b02001186f45604dc77b46a2fd5
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-06 10:45:23 +0100

    Shared symbols use case

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+console.log('::SHARED SYMBOLS::');
+// Shared symbols/id:s with Symbol.for
+let symbol1 = Symbol.for('age');
+let symbol2 = Symbol.for('age');
+console.log('Are they the same?', symbol1 == symbol2);
+
+// Use case
+let person = {
+    name: 'Hannes'
+}
+
+function makeAge(person) {
+    let ageSymbol = Symbol.for('age'); // Not accessible inside function
+    person[ageSymbol] = 27;
+}
+
+makeAge(person);
+
+console.log(person[symbol1]); // Symbol1 has same ID/KEY ("age") and therefore accessible
+
+
 console.log('::SYMBOLS::');
 // Type = Symbol
 let symbol = Symbol('debug'); // "debug" var only for id:ing/debugging
