8008bb80f094b78281e17296e0a3f50403f6df73
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-13 18:43:02 +0100

    Looping entries, vals and keys: values(), keys()

--- script.js
+++ script.js
@@ -15,7 +15,20 @@ console.log('deck', deck);
 console.log('deck.size', deck.size);
 console.log('deck.get', deck.get('as'));
 
+// Keys
+console.log('key()');
+for ( key of deck.keys() ) { // See keys that are stored in map
+    console.log('key loop', key);
+}
+for ( value of deck.values() ) {
+    console.log('value loop', value); // See values stored in map
+}
+for ( entry of deck ) { // equivilent to deck.entries()
+    console.log('entry loop', entry); // See whole object
+}
+
 deck.delete('as'); // Clear item with .delete
 console.log('deck', deck);
 deck.clear('as'); // Clear ALL items with .clear
 console.log('deck', deck);
+
