5f0c3cc03a9f46880f6c4c556f411919d2244e81
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-13 18:32:11 +0100

    Getters and Setters

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Script running...');
+console.log('MAP() - Get:ers and Set:ers');
+
+let cardAce = {
+    name: 'Ace of Spades'
+};
+let cardKing = {
+    name: 'King of Hearts'
+};
+
+let deck = new Map();
+deck.set('as', cardAce); // Set key to an object. Extract with .get
+deck.set('kc', cardKing);
+
+console.log('deck', deck);
+console.log('deck.size', deck.size);
+console.log('deck.get', deck.get('as'));
+
+deck.delete('as'); // Clear item with .delete
+console.log('deck', deck);
+deck.clear('as'); // Clear ALL items with .clear
+console.log('deck', deck);
