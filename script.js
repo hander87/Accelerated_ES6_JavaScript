c2699ef6517d6b256dad4f6af7a287f213b74b60
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-13 16:03:04 +0100

    Basic Promise Succeed

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Script running...');
+console.log('PROMISES');
+
+let promise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve('Promise Done!');
+    }, 1500);
+});
+
+promise.then( (value) => {
+    console.log('1,5 second: ', value)
+});
