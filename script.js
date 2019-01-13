adbb1e02d15ad2dfb1e254f626be44331a62fbbf
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-13 16:08:09 +0100

    Promise Failed

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-console.log('PROMISES');
 
-let promise = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        resolve('Promise Done!');
+
+let promise2 = new Promise((resolve, reject) => {
+    setTimeout( () => {
+        reject('Promise Failed!');
+    }, 1500);
+});
+
+promise2.then( (value) => {
+    console.log('1,5 second: ', value);
+}, (error) => {
+    console.log('PROMISE FAILED');
+    console.log('error', error)
+});
+
+
+
+
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout( () => {
+        console.log('PROMISE SUCCEED');
+        resolve('Promise Succeed!');
     }, 1500);
 });
 
 promise.then( (value) => {
-    console.log('1,5 second: ', value)
+    console.log('1,5 second: ', value);
+}, (error) => {
+    console.log('error', error)
 });
