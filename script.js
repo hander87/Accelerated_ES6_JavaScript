309edbd44f06d7a66d46814ab83c6d1a71f0ead2
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2019-01-19 12:49:37 +0100

    entires() provide key value in Set()

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ set.add(3); // Adds values with .add()
 set.delete(1); // Deletes the value (even though you have "duplicates")
 console.log( set.has(1) ); // instead of "get"
 
+for (const item of set.entries()) { // This provides the "key"/position value
+    console.log(item);
+}
+
 for (const item of set) {
     console.log(item);
 }
+
 
 
 
